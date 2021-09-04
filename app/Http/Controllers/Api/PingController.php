3f0e3ee897a2d8e47a6901fcd9ad769dd4bfb777
yaozm <ityaozm@gmail.com>
2021-09-04 19:21:46 +0800

    添加 api docs

--- app/Http/Controllers/Api/PingController.php
+++ app/Http/Controllers/Api/PingController.php
@@ -4,8 +4,32 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 
+/**
+ * @group Example management
+ *
+ * APIs for managing Example
+ */
 class PingController extends Controller
 {
+    /**
+     * This is a successful example.
+     *
+     * @queryParam sort string Field to sort by. Defaults to 'id'.
+     *
+     * @urlParam id integer required The ID of the post.
+     *
+     * @bodyParam user_id int required The id of the user. Example: 9
+     *
+     * @response {
+     *     "status": "success",
+     *     "code": 200,
+     *     "message": "Http ok",
+     *     "data": [
+     *             "This is a successful example."
+     *         ],
+     *     "error": {}
+     * }
+     */
     public function ping(Request $request)
     {
         if ($request->get('is_bad')) {
