c0e0bc7fcf711e7e52e316743a7e16b576fa8fbd
yaozm <ityaozm@gmail.com>
2021-11-09 17:11:59 +0800

    添加 base64 验证规则

---
+++ app/Rules/Base64Rule.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Rules;
+
+class Base64Rule extends Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return base64_encode(base64_decode($value, true)) === $value;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute must be a valid base64 string.';
+    }
+}
