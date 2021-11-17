25bee79c6a775bbc02144cd55f806d96df000e84
yaozm <ityaozm@gmail.com>
2021-11-17 10:10:02 +0800

    添加 `symfony/stopwatch`

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,5 +1,36 @@
 <?php
 
+use Illuminate\Support\Facades\App as Laravel;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+if (! function_exists('stopwatch')) {
+    /**
+     * @param  string  $name
+     * @param  callable|string $callback
+     * @param  array  $parameters
+     * @param  null|string  $category
+     * @param  bool  $morePrecision
+     * @param  null|\Psr\Log\LoggerInterface  $logger
+     *
+     * @return mixed
+     */
+    function stopwatch(string $name, $callback, array $parameters = [], string $category = null, bool $morePrecision = false, LoggerInterface $logger = null)
+    {
+        $stopwatch = new Stopwatch($morePrecision);
+
+        $logger or $logger = Laravel::make(LoggerInterface::class);
+
+        $stopwatch->start($name, $category) and $logger->info("Start stopwatch [$name]");
+
+        $called = Laravel::call($callback, $parameters);
+
+        $stopwatchEvent = $stopwatch->stop($name) and $logger->info("End stopwatch [$name] [$stopwatchEvent]");
+
+        return $called;
+    }
+}
+
 if (! function_exists('array_reduces')) {
     /**
      * @param  array  $array
