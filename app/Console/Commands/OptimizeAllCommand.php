cd587b3bfe471e15f88f06610f0b97b029d63607
yaozm <ityaozm@gmail.com>
2021-11-17 14:55:10 +0800

    重命名 OptimizeEnvironmentCommand.php -> OptimizeAllCommand.php

--- app/Console/Commands/OptimizeAllCommand.php
+++ app/Console/Commands/OptimizeAllCommand.php
@@ -4,21 +4,21 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class OptimizeEnvironmentCommand extends Command
+class OptimizeAllCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'optimize:environment {--f|force : Force optimize.}';
+    protected $signature = 'optimize:all {--f|force : Force optimize.}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Optimize environment.';
+    protected $description = 'Optimize all.';
 
     /**
      * Create a new command instance.
@@ -41,19 +41,11 @@ class OptimizeEnvironmentCommand extends Command
             return;
         }
 
-        $this->call('config:clear');
-        $this->call('config:cache');
-
-        $this->call('route:clear');
-        $this->call('route:cache');
-
-        $this->call('view:clear');
-        $this->call('view:cache');
-
-        $this->call('clear-compiled');
-        $this->call('optimize');
-
         passthru('composer dump-autoload --optimize');
+        $this->call('config:cache');
+        $this->call('event:cache');
+        $this->call('route:cache');
+        $this->call('view:cache');
 
         return self::SUCCESS;
     }
