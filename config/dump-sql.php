3c13c48cb9d373f65b2bfdd7b945b5387c7d7317
yaozm <ityaozm@gmail.com>
2021-11-23 11:01:06 +0800

    升级辅助开发包

---
+++ config/dump-sql.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the guanguans/laravel-dump-sql.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+return [
+    /*
+     * The host to use when listening for debug server connections.
+     */
+    'host' => env('LISTEN_SQL_SERVER_HOST', 'tcp://127.0.0.1:9913'),
+];
