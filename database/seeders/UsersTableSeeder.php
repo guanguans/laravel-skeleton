1dd72bd4f51154c306acfa366cd4ab5e6ace8e7f
yaozm <ityaozm@gmail.com>
2021-12-03 16:06:45 +0800

    添加用户表数据填充种子

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('users')->delete();
+
+        \DB::table('users')->insert([
+            [
+                'email' => 'admin@admin.com',
+                'email_verified_at' => '2021-11-18 18:10:18',
+                'name' => 'admin',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'remember_token' => 'bkErVvxVox',
+                'created_at' => '2021-11-18 18:10:18',
+                'updated_at' => '2021-11-18 18:10:18',
+            ],
+        ]);
+    }
+}
