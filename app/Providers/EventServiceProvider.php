d0b93b2809f6266dbed2f2f974f9bcc60b8c239d
yaozm <ityaozm@gmail.com>
2021-12-03 17:55:54 +0800

    Fix style

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -35,7 +35,6 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
         $this->registerObservers();
     }
 
@@ -44,9 +43,9 @@ class EventServiceProvider extends ServiceProvider
      */
     protected function registerObservers(): void
     {
-        /* @var \Illuminate\Database\Eloquent\Model $class */
-        foreach ($this->observers as $class => $observer) {
-            $class::observe($observer);
+        /** @var \Illuminate\Database\Eloquent\Model $modelClass */
+        foreach ($this->observers as $modelClass => $observer) {
+            $modelClass::observe($observer);
         }
     }
 }
