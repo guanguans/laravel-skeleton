af2d05ab66170038a0d4ee5ea85a382f5a535c92
yaozm <ityaozm@gmail.com>
2021-12-22 10:50:45 +0800

    添加自动部署失败钉钉通知命令

---
+++ app/Console/Commands/DeployerFailedCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DeployerFailedCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'deployer:failed';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deployer failed';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // todo something
+        notifier_report($this->description);
+
+        return 0;
+    }
+}
