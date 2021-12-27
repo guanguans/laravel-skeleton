6d3e8d0157d38d0e2de569638432115407fbcd99
yaozm <ityaozm@gmail.com>
2021-12-27 10:21:01 +0800

    重命名 `CopyAble` -> `Copyable`

--- app/Traits/Copyable.php
+++ app/Traits/Copyable.php
@@ -2,7 +2,7 @@
 
 namespace App\Traits;
 
-trait CopyAble
+trait Copyable
 {
     public function copy()
     {
--- app/Traits/Filterable.php
+++ app/Traits/Filterable.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 /**
  * @method static filter(array $input = [])
  */
-trait FilterAble
+trait Filterable
 {
     public function scopeFilter(Builder $query, ?array $input = null)
     {
