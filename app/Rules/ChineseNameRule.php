227950b7e474e7bd268fe7b924ce627156916557
yaozm <ityaozm@gmail.com>
2021-12-29 14:21:43 +0800

    添加中文姓名验证规则

---
+++ app/Rules/ChineseNameRule.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Rules;
+
+class ChineseNameRule extends RegexRule
+{
+    /**
+     * 中文姓名.
+     *
+     * @return string
+     */
+    protected function pattern(): string
+    {
+        return '/^(?:[\u4e00-\u9fa5·]{2,16})$/';
+    }
+}
