8da38ab909e6c45d228ea4a0f6047a17a4099dab
yaozm <ityaozm@gmail.com>
2022-04-28 14:46:34 +0800

    规范api统一响应

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -2,31 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Resources\Json\JsonResource;
-use Jiannei\Response\Laravel\Response;
+use Jiannei\Response\Laravel\Support\Traits\JsonResponseTrait;
 
-/**
- * @method JsonResponse|JsonResource accepted($data = null, string $message = '', string $location = '')
- * @method JsonResponse|JsonResource created($data = null, string $message = '', string $location = '')
- * @method JsonResponse|JsonResource noContent(string $message = '')
- * @method JsonResponse|JsonResource localize(int $code = 200, array $headers = [], int $option = 0)
- * @method JsonResponse|JsonResource ok(string $message = '', int $code = 200, array $headers = [], int $option = 0)
- * @method JsonResponse|JsonResource success($data = null, string $message = '', int $code = 200, array $headers = [], int $option = 0)
- * @method JsonResponse|JsonResource errorBadRequest(?string $message = '')
- * @method JsonResponse|JsonResource errorUnauthorized(string $message = '')
- * @method JsonResponse|JsonResource errorForbidden(string $message = '')
- * @method JsonResponse|JsonResource errorNotFound(string $message = '')
- * @method JsonResponse|JsonResource errorMethodNotAllowed(string $message = '')
- * @method JsonResponse|JsonResource errorInternal(string $message = '')
- * @method JsonResponse|JsonResource fail(string $message = '', int $code = 500, $errors = null, array $header = [], int $options = 0)
- *
- * @see \Jiannei\Response\Laravel\Response
- */
 class Controller extends \App\Http\Controllers\Controller
 {
-    public function __call($name, $arguments)
-    {
-        return app(Response::class)->{$name}(...$arguments);
-    }
+    use JsonResponseTrait;
 }
