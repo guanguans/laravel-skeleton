b5ba7aa28ff1927fef62d88b43ee4851a80bde15
yaozm <ityaozm@gmail.com>
2022-05-11 17:32:36 +0800

    新增通用布尔枚举类

---
+++ app/Enums/BooleanEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static False()
+ * @method static static True()
+ */
+final class BooleanEnum extends Enum
+{
+    public const False = false;
+    public const True = true;
+}
