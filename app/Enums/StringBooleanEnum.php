8f4d9fcc0ab6021b06472351f21546e38a2d4920
yaozm <ityaozm@gmail.com>
2022-05-11 17:33:27 +0800

    新增通用整型布尔枚举类

---
+++ app/Enums/StringBooleanEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static False()
+ * @method static static True()
+ */
+final class StringBooleanEnum extends Enum
+{
+    public const False = '0';
+    public const True = '1';
+}
