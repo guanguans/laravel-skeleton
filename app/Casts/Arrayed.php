1b9acbe5f10de87a9a12967f05844a26645ada74
yaozm <ityaozm@gmail.com>
2022-05-17 15:31:08 +0800

    新增模型字段数组cast转换类

---
+++ app/Casts/Arrayed.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class Arrayed implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return array
+     */
+    public function get($model, $key, $value, $attributes)
+    {
+        if (! $value) {
+            return [];
+        }
+
+        $value = @json_decode($value, true);
+
+        return $value ?: [];
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  array  $value
+     * @param  array  $attributes
+     * @return string
+     */
+    public function set($model, $key, $value, $attributes)
+    {
+        return json_encode($value);
+    }
+}
