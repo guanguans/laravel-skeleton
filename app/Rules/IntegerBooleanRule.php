d04f187d56dda6c9139279accd654439f9ce80e6
yaozm <ityaozm@gmail.com>
2022-05-23 11:58:26 +0800

    新增数字类型布尔验证规则

---
+++ app/Rules/IntegerBooleanRule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Rules;
+
+final class IntegerBooleanRule extends Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $this->attribute = $attribute;
+
+        return in_array($value, [0, 1]);
+    }
+}
