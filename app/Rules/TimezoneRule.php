cfdc499bf985f746656d1b258143515c718ff555
yaozm <ityaozm@gmail.com>
2022-07-07 15:39:20 +0800

    新增 Timezone 时区验证规则

---
+++ app/Rules/TimezoneRule.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Rules;
+
+use DateTimeZone;
+
+final class TimezoneRule extends Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $this->attribute = $attribute;
+
+        return in_array($value, DateTimeZone::listIdentifiers());
+    }
+}
