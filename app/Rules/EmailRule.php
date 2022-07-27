a39c6e4e39f293447e777f542e7e003e128992f1
yaozm <ityaozm@gmail.com>
2022-07-27 16:29:17 +0800

    新增邮箱验证规则

---
+++ app/Rules/EmailRule.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Rules;
+
+final class EmailRule extends RegexRule
+{
+    protected function pattern(): string
+    {
+        return '/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+    }
+}
