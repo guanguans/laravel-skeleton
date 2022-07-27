bf32e5e0286b2839cac50a9083c132fef054447c
yaozm <ityaozm@gmail.com>
2022-07-27 16:16:11 +0800

    更新手机号验证规则

--- app/Rules/PhoneRule.php
+++ app/Rules/PhoneRule.php
@@ -6,6 +6,6 @@ final class PhoneRule extends RegexRule
 {
     protected function pattern(): string
     {
-        return '/^1(3[0-9]|4[57]|5[0-35-9]|6[6]|7[0135678]|8[0-9])\d{8}$/';
+        return '/^(?:(?:\+|00)86)?1[3-9]\d{9}$/';
     }
 }
