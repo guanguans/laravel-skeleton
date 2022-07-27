14ff83e45b3dca640b4a5b1bb03b92caa3c80ac9
yaozm <ityaozm@gmail.com>
2022-07-27 16:19:35 +0800

    更新密码验证规则

--- app/Rules/StrongPassword.php
+++ app/Rules/StrongPassword.php
@@ -6,6 +6,7 @@ final class StrongPassword extends RegexRule
 {
     protected function pattern(): string
     {
-        return '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@()$%^&*=_{}[\]:;"\'|\\<>,.\/~`±§+-]).{12,30}$/';
+        // 最少6位，包括至少1个大写字母，1个小写字母，1个数字，1个特殊字符
+        return '/^\S*(?=\S{8,})(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*[!@#$%^&*? ])\S*$/';
     }
 }
