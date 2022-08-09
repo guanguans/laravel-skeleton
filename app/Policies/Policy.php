e39554184d34d33e2f602ebfde3ea462deaa0749
yaozm <ityaozm@gmail.com>
2022-08-09 18:04:16 +0800

    优化基础策略类

--- app/Policies/Policy.php
+++ app/Policies/Policy.php
@@ -4,10 +4,22 @@ namespace App\Policies;
 
 use App\Models\JWTUser;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\Gate;
 
 class Policy
 {
     use HandlesAuthorization;
+
+    /**
+     * Retrieves all registered policies from the Gate. Only policies registered in the application
+     * can be assigned to groups.
+     *
+     * @return array
+     */
+    public static function all()
+    {
+        return array_keys(Gate::abilities());
+    }
 
     public function before(JWTUser $user)
     {
