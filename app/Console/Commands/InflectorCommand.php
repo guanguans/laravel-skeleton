b38d5dc6f3ab7281e7a28ce6ef6cacaffae6f0df
yaozm <ityaozm@gmail.com>
2022-08-10 11:47:06 +0800

    `while(true)` -> `for(;;)`

--- app/Console/Commands/InflectorCommand.php
+++ app/Console/Commands/InflectorCommand.php
@@ -43,7 +43,14 @@ class InflectorCommand extends Command
     {
         collect()
             ->pipe(function (Collection $collection) {
-                while (true) {
+                // while (true) {
+                //     $phrase = $this->ask('Please enter a phrase to inflect.');
+                //     if (filled($phrase)) {
+                //         break;
+                //     }
+                // }
+
+                for (;;) {
                     $phrase = $this->ask('Please enter a phrase to inflect.');
                     if (filled($phrase)) {
                         break;
