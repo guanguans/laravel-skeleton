05b6a905dd64557be83b295097452392b154d928
yaozm <ityaozm@gmail.com>
2022-08-29 10:13:12 +0800

    新增仓库 `app/Support/Repository`

---
+++ app/Support/Repository.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Support;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class Repository
+{
+    /**
+     * @var \Illuminate\Database\Eloquent\Model
+     */
+    protected $model;
+
+    /**
+     * Repository constructor.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @param  array  $columns
+     * @param  array  $relations
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function all(array $columns = ['*'], array $relations = []): Collection
+    {
+        return $this->model->with($relations)->get($columns);
+    }
+
+    /**
+     * Get all trashed models.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function allTrashed(): Collection
+    {
+        return $this->model->onlyTrashed()->get();
+    }
+
+    /**
+     * Find model by id.
+     *
+     * @param  int  $modelId
+     * @param  array  $columns
+     * @param  array  $relations
+     * @param  array  $appends
+     *
+     * @return null|\Illuminate\Database\Eloquent\Model
+     */
+    public function findById(int $modelId, array $columns = ['*'], array $relations = [], array $appends = []): ?Model
+    {
+        return $this->model->select($columns)->with($relations)->findOrFail($modelId)->append($appends);
+    }
+
+    /**
+     * Find trashed model by id.
+     *
+     * @param  int  $modelId
+     *
+     * @return null|\Illuminate\Database\Eloquent\Model
+     */
+    public function findTrashedById(int $modelId): ?Model
+    {
+        return $this->model->withTrashed()->findOrFail($modelId);
+    }
+
+    /**
+     * Find only trashed model by id.
+     *
+     * @param  int  $modelId
+     *
+     * @return null|\Illuminate\Database\Eloquent\Model
+     */
+    public function findOnlyTrashedById(int $modelId): ?Model
+    {
+        return $this->model->onlyTrashed()->findOrFail($modelId);
+    }
+
+    /**
+     * Create a model.
+     *
+     * @param  array  $payload
+     *
+     * @return null|\Illuminate\Database\Eloquent\Model
+     */
+    public function create(array $payload): ?Model
+    {
+        $model = $this->model->create($payload);
+
+        return $model->fresh();
+    }
+
+    /**
+     * Update existing model.
+     *
+     * @param  int  $modelId
+     * @param  array  $payload
+     *
+     * @return bool
+     */
+    public function update(int $modelId, array $payload): bool
+    {
+        $model = $this->findById($modelId);
+
+        return $model->update($payload);
+    }
+
+    /**
+     * Delete model by id.
+     *
+     * @param  int  $modelId
+     *
+     * @return bool
+     */
+    public function deleteById(int $modelId): bool
+    {
+        return $this->findById($modelId)->delete();
+    }
+
+    /**
+     * Restore model by id.
+     *
+     * @param  int  $modelId
+     *
+     * @return bool
+     */
+    public function restoreById(int $modelId): bool
+    {
+        return $this->findOnlyTrashedById($modelId)->restore();
+    }
+
+    /**
+     * Permanently delete model by id.
+     *
+     * @param  int  $modelId
+     *
+     * @return bool
+     */
+    public function permanentlyDeleteById(int $modelId): bool
+    {
+        return $this->findTrashedById($modelId)->forceDelete();
+    }
+}
