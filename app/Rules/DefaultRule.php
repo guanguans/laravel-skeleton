67540018bdec7aeb61b2d030ecc86c51cbc32075
yaozm <ityaozm@gmail.com>
2022-09-14 11:28:32 +0800

    新增独立默认值规则

---
+++ app/Rules/DefaultRule.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Rules;
+
+use App\Rules\Concerns\DataAware;
+use App\Rules\Concerns\ValidatorAware;
+use Illuminate\Contracts\Validation\DataAwareRule;
+use Illuminate\Contracts\Validation\ValidatorAwareRule;
+
+final class DefaultRule extends ImplicitRule implements DataAwareRule, ValidatorAwareRule
+{
+    use DataAware;
+    use ValidatorAware;
+
+    /**
+     * @var null|mixed
+     */
+    protected $default;
+
+    public function __construct($default = null)
+    {
+        $this->default = $default;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if ($value !== null && $value !== '') {
+            return true;
+        }
+
+        $default = $this->default ?: $value;
+        $this->data[$attribute] = $default;
+        $this->validator->setData($this->data);
+
+        return true;
+    }
+}
