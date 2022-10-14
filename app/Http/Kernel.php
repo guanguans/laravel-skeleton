1fcb3704f361b8fbddac33d7d33a139c669e62da
yaozm <ityaozm@gmail.com>
2022-10-14 11:02:17 +0800

    添加重定向 Laravel 应用程序中缺失的页面

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         // \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         // \Spatie\HttpLogger\Middlewares\HttpLogger::class,
+        \Spatie\MissingPageRedirector\RedirectsMissingPages::class,
     ];
 
     /**
