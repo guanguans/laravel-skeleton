ceeeb92d977a226e2a043d1975f0bc378a814144
yaozm <ityaozm@gmail.com>
2022-10-18 16:51:37 +0800

    添加统一 Json 响应辅助包

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
+use F9Web\ApiResponseHelpers;
 use Jiannei\Response\Laravel\Support\Traits\JsonResponseTrait;
 
 class Controller extends \App\Http\Controllers\Controller
 {
     use JsonResponseTrait;
+    use ApiResponseHelpers;
 }
