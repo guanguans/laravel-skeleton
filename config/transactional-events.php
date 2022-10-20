b2d0774a3e12095520e6c2a18e3502a4955a1751
yaozm <ityaozm@gmail.com>
2022-10-20 11:57:15 +0800

    新增Laravel 的事务感知事件调度器包

---
+++ config/transactional-events.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Transactional Events Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This file is for configuring the transactional events of your application
+    | that should be dispatched if and only if the outer transaction commits.
+    | You can enable event namespaces using prefixes such as App\\ as well
+    | as setting up events that should not have a transactional behavior.
+    |
+    */
+
+    'enable' => false,
+
+    'transactional' => [
+        'App\Events',
+    ],
+
+    'excluded' => [
+        // 'eloquent.*',
+        'eloquent.booted',
+        'eloquent.retrieved',
+        'eloquent.saved',
+        'eloquent.updated',
+        'eloquent.created',
+        'eloquent.deleted',
+        'eloquent.restored',
+    ],
+];
