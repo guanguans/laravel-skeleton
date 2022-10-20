a19b41abd8d98010251c342f9da501b7df8be9ce
yaozm <ityaozm@gmail.com>
2022-10-20 11:29:04 +0800

    新增当前用户密码验证规则

---
+++ app/Rules/CurrentUserPasswordRule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Support\Facades\Hash;
+
+final class CurrentUserPasswordRule extends Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return Hash::check($value, auth()->user()->password);
+    }
+}
