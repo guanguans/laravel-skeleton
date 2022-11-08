6f31aa4c88d46dc117dd7a8ceac944b547cc61d4
yaozm <ityaozm@gmail.com>
2022-11-08 14:50:56 +0800

    新增 `PushDeer` 门面

---
+++ app/Support/Facades/PushDeer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Support\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class PushDeer extends Facade
+{
+    /**
+     * @inheritdoc
+     */
+    protected static function getFacadeAccessor()
+    {
+        return \App\Support\PushDeer::class;
+    }
+}
