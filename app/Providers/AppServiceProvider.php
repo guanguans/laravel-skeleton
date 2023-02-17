7dd2900099371d0f2ee4e3a27daa857a20819dfc
yaozm <ityaozm@gmail.com>
2023-02-17 10:20:26 +0800

    Carbon 序列化使用 `Y-m-d H:i:s` 格式

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -98,7 +98,11 @@ class AppServiceProvider extends ServiceProvider
         $this->while(true, function () {
             // 低版本 MySQL(< 5.7.7) 或 MariaDB(< 10.2.2)，则可能需要手动配置迁移生成的默认字符串长度，以便按顺序为它们创建索引。
             Schema::defaultStringLength(191);
-            Carbon::setLocale(config('app.locale'));
+            $this->app->setLocale($locale = config('app.locale'));
+            Carbon::setLocale($locale);
+            Carbon::serializeUsing(function (Carbon $timestamp) {
+                return $timestamp->format('Y-m-d H:i:s');
+            });
             // JsonResource::wrap('data');
             JsonResource::withoutWrapping();
             Paginator::useBootstrap();
