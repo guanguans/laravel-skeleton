4bf86c58a9d8a88ffd64d1593e4f82b5f6a863df
yaozm <ityaozm@gmail.com>
2023-05-14 17:00:01 +0800

    Optimize sse

--- app/Support/Sse/ServerSentEvent.php
+++ app/Support/Sse/ServerSentEvent.php
@@ -5,7 +5,64 @@ declare(strict_types=1);
 namespace App\Support\Sse;
 
 /**
+ * ```php
+ * Route::get('stream', static function (): Symfony\Component\HttpFoundation\StreamedResponse {
+ *     return new Symfony\Component\HttpFoundation\StreamedResponse(
+ *         new App\Support\Sse\ServerSentEvent(static function (App\Support\Sse\ServerSentEvent $serverSentEvent): void {
+ *             $serverSentEvent
+ *                 ->setEvent($event = (static function (): string {
+ *                     $events = ['', 'message', 'notice'];
+ *                     $index = array_rand($events, 1);
+ *
+ *                     return $events[$index];
+ *                 })())
+ *                 ->setData(['event' => $event, 'time' => time()])
+ *                 ->setId(uniqid('', true))
+ *                 ->setComment('comment')
+ *                 ->setRetry(3000)
+ *                 ->setSleep(3);
+ *         }),
+ *         Symfony\Component\HttpFoundation\Response::HTTP_OK,
+ *         ['Access-Control-Allow-Origin' => '*']
+ *     );
+ * });
+ * ```
+ *
+ * ```guzzle
+ * (new GuzzleHttp\Client())->get(
+ *     'https://strangecat-api.test/stream',
+ *     [
+ *         'curl' => [
+ *             CURLOPT_WRITEFUNCTION => static function (object $ch, string $data) {
+ *                 dump($data);
+ *
+ *                 return strlen($data);
+ *             },
+ *         ],
+ *     ]
+ * );
+ * ```
+ *
+ * ```js
+ * const source = new EventSource('https://strangecat-api.test/stream');
+ * source.onopen = event => console.log('onopen', event);
+ * source.onerror = event => console.log('onerror', event);
+ * source.onmessage = event => {
+ *     console.log(event.data);
+ *     // source.close(); // Close event stream.
+ * };
+ * source.addEventListener('notice', event => {
+ *     console.log(event.data);
+ *     // source.close(); // Close event stream.
+ * });
+ * ```
+ *
+ * @see https://developer.mozilla.org/zh-CN/docs/Web/API/EventSource
  * @see https://developer.mozilla.org/zh-CN/docs/Web/API/Server-sent_events/Using_server-sent_events
+ * @see https://github.com/mdn/dom-examples/tree/main/server-sent-events
+ * @see https://github.com/hhxsv5/php-sse
+ * @see https://github.com/sarfraznawaz2005/laravel-sse
+ * @see https://github.com/qruto/laravel-wave
  */
 class ServerSentEvent implements \Stringable
 {
