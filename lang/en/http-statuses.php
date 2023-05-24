ed42977c8d0e16575a68774b94c2366649c6f30e
yaozm <ityaozm@gmail.com>
2023-05-24 17:01:33 +0800

    Upgrade to laravel 10

--- lang/en/http-statuses.php
+++ lang/en/http-statuses.php
@@ -29,7 +29,7 @@ return [
     '401' => 'Unauthorized',
     '402' => 'Payment Required',
     '403' => 'Forbidden',
-    '404' => 'Page Not Found',
+    '404' => 'Not Found',
     '405' => 'Method Not Allowed',
     '406' => 'Not Acceptable',
     '407' => 'Proxy Authentication Required',
