e7aa8a88ba8b9f2b4d45ae161b357f9af55d55aa
yaozm <ityaozm@gmail.com>
2023-07-05 11:55:08 +0800

    chore(deps): add plannr/laravel-fast-refresh-database dependency
    - Added plannr/laravel-fast-refresh-database dependency

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,14 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Plannr\Laravel\FastRefreshDatabase\Traits\FastRefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    // use RefreshDatabase;
+    use FastRefreshDatabase;
 }
