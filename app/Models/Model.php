516b9c4962c320886d4043d76775a7e63307c621
yaozm <ityaozm@gmail.com>
2023-07-06 10:33:37 +0800

    feat(Model): add toDotArray() method
    - Add `toDotArray()` method to convert model to dot array
    - Use `Arr::dot()` to flatten the array

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use App\Models\Concerns\AllowedFilterable;
@@ -9,6 +11,7 @@ use App\Models\Concerns\Observable;
 use App\Models\Concerns\Pipeable;
 use App\Models\Concerns\SerializeDate;
 use App\Models\Concerns\Sortable;
+use Illuminate\Support\Arr;
 use Watson\Validating\ValidatingTrait;
 
 class Model extends \Illuminate\Database\Eloquent\Model
@@ -23,4 +26,9 @@ class Model extends \Illuminate\Database\Eloquent\Model
     use ValidatingTrait;
 
     protected $rules = [];
+
+    public function toDotArray(): array
+    {
+        return Arr::dot($this->toArray());
+    }
 }
