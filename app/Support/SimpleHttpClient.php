2b1926175ea0428dd66405e431a92ba834f0f96d
yaozm <ityaozm@gmail.com>
2023-07-28 11:40:15 +0800

    refactor(http-client): update SimpleHttpClient constructor
    - Add options parameter to the constructor
    - Set options using the setOptions method

--- app/Support/SimpleHttpClient.php
+++ app/Support/SimpleHttpClient.php
@@ -11,39 +11,46 @@ use Psr\Http\Message\ResponseInterface;
 
 class SimpleHttpClient implements ClientInterface
 {
-    public function sendRequest(RequestInterface $request): ResponseInterface
+    public function __construct(private array $options = [])
     {
-        $streamContextOptions = [
-            'protocol_version' => $request->getProtocolVersion(),
-            'method' => $request->getMethod(),
-            'header' => $this->toLineHeaders($request),
-            'timeout' => 30,
+        $this->setOptions($options);
+    }
+
+    public function setOptions(array $options): self
+    {
+        $this->options = $options + [
+            'method' => 'GET',
+            'header' => [],
+            'user_agent' => \ini_get('user_agent'),
+            'content' => '',
+            'proxy' => '',
+            'request_fulluri' => false,
+            'follow_location' => 1,
+            'max_redirects' => 20,
+            'protocol_version' => '1.1',
+            'timeout' => \ini_get('default_socket_timeout'),
             'ignore_errors' => true,
-            'follow_location' => 0,
-            'max_redirects' => 100,
-            'content' => (string) $request->getBody(),
         ];
 
-        $streamContext = stream_context_create([
-            'http' => $streamContextOptions,
-            'https' => $streamContextOptions,
-            'ssl' => [
-                'verify_peer' => false,
-            ],
-        ]);
+        return $this;
+    }
 
-        /*
-         * file_get_contents() will issue PHP warnings, these should be converted to exceptions thus
-         * set_error_handler() / restore_error_handler() is used
-         */
-        set_error_handler(static function ($errno, $errstr, $errfile, $errline, $errcontext) use (&$errors): void {
-            $errors[] = $errstr;
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        set_error_handler(static function (int $errno, string $errstr, ?string $errfile = null, ?int $errline = null) use (&$errors): void {
+            // Warning: file_get_contents(/api/any): Failed to open stream: No such file or directory in /Users/yaozm/Documents/wwwroot/laravel-skeleton/app/Support/SimpleHttpClient.php on line 25
+            $errors[] = sprintf('%s: %s in %s on line %s', $errno, $errstr, $errfile, $errline);
         });
-        $responseBody = file_get_contents((string) $request->getUri(), false, $streamContext);
+        $responseBody = file_get_contents((string) $request->getUri(), false, $this->toStreamContext($request));
         restore_error_handler();
 
         if (false === $responseBody && $errors) {
-            throw new \RuntimeException(implode('; ', $errors));
+            throw new \RuntimeException(implode(PHP_EOL, $errors));
         }
 
         $http = $this->toHttp($http_response_header);
@@ -57,7 +64,7 @@ class SimpleHttpClient implements ClientInterface
         );
     }
 
-    private function toHttp(array $http_response_header)
+    private function toHttp(array $http_response_header): array
     {
         /** @var array $http */
         $http = explode(' ', $http_response_header[0]);
@@ -69,9 +76,9 @@ class SimpleHttpClient implements ClientInterface
         ];
     }
 
-    private function toLineHeaders(RequestInterface $request): string
+    private function toIndexHeaders(RequestInterface $request): array
     {
-        $headers = array_reduce(
+        return array_reduce(
             array_keys($request->getHeaders()),
             static function (array $headers, string $name) use ($request): array {
                 $values = 'content-length' === strtolower($name)
@@ -86,11 +93,9 @@ class SimpleHttpClient implements ClientInterface
             },
             []
         );
-
-        return implode("\r\n", $headers);
     }
 
-    private function toAssocHeaders(array $http_response_header)
+    private function toAssocHeaders(array $http_response_header): array
     {
         $sterilizedLineHeaders = \array_slice($http_response_header, 1);
 
@@ -102,5 +107,17 @@ class SimpleHttpClient implements ClientInterface
             1,
             0
         );
+    }
+
+    private function toStreamContext(RequestInterface $request)
+    {
+        $options = [
+            'method' => $request->getMethod(),
+            'header' => $this->toIndexHeaders($request),
+            'content' => (string) $request->getBody(),
+            'protocol_version' => $request->getProtocolVersion(),
+        ] + $this->options;
+
+        return stream_context_create(['http' => $options]);
     }
 }
