485e50389ea9ea562ad41fb655b5790aef97468f
yaozm <ityaozm@gmail.com>
2023-07-29 20:22:53 +0800

    fix(psrclient): handle file_get_contents error
    - Change the variable name from 'errors' to 'error' in the error handling callback function
    - Assign the value of 'errstr' to the 'error' variable instead of adding it to an array
    - Throw a RuntimeException with the 'error' message if file_get_contents fails

--- app/Support/Http/PsrClient.php
+++ app/Support/Http/PsrClient.php
@@ -24,32 +24,65 @@ class PsrClient implements ClientInterface
      */
     public function sendRequest(RequestInterface $request): ResponseInterface
     {
-        $uri = (string) $request->getUri();
-        $streamContext = $this->toStreamContext($request);
-
         set_error_handler(static function (
             int $errno,
             string $errstr,
             ?string $errfile = null,
             ?int $errline = null
-        ) use (&$errors): void {
+        ) use (&$error): void {
             // Warning: file_get_contents(/api/any): Failed to open stream: No such file or directory in /...Client.php on line 25
             // $error = "Errno {$errno}: {$errstr}";
             // $errfile and $error .= " in {$errfile}";
             // $errline and $error .= " on line {$errline}";
-            // $errors[] = $error;
-            $errors[] = $errstr;
+            $error = $errstr;
         });
-        $responseBody = file_get_contents($uri, false, $streamContext);
+        $responseBody = file_get_contents((string) $request->getUri(), false, $this->toStreamContext($request));
         restore_error_handler();
 
-        if (false === $responseBody && $errors) {
-            throw new \RuntimeException(implode(PHP_EOL, $errors));
+        if (false === $responseBody && $error) {
+            throw new \RuntimeException($error);
         }
 
         [$version, $status, $reason, $headers] = HeaderProcessor::parseHeaders($http_response_header);
 
         return new Response($status, $headers, $responseBody, $version, $reason);
+    }
+
+    private static function toProgressNotification(callable $process): \Closure
+    {
+        return static function (
+            int $notificationCode,
+            int $severity,
+            string $message,
+            int $messageCode,
+            int $bytesTransferred,
+            int $bytesMax
+        ) use ($process): void {
+            if (STREAM_NOTIFY_PROGRESS === $notificationCode) {
+                // https://www.php.net/manual/zh/function.stream-notification-callback.php#121236
+                $bytesTransferred > 0 and $bytesTransferred += 8192;
+                $process($bytesMax, $bytesTransferred);
+            }
+        };
+    }
+
+    private static function toIndexHeaders(RequestInterface $request): array
+    {
+        return array_reduce(
+            array_keys($request->getHeaders()),
+            static function (array $headers, string $name) use ($request): array {
+                $values = 'content-length' === strtolower($name)
+                    ? [\strlen((string) $request->getBody())]
+                    : $request->getHeader($name);
+
+                foreach ($values as $value) {
+                    $headers[] = "{$name}: {$value}";
+                }
+
+                return $headers;
+            },
+            []
+        );
     }
 
     private function configureDefaults(array $config): void
@@ -79,58 +112,21 @@ class PsrClient implements ClientInterface
         }
 
         if (\is_callable($config['progress'])) {
-            $config['notification'] = $this->progressNotification($config['progress']);
+            $config['notification'] = self::toProgressNotification($config['progress']);
         }
 
         $this->config = $config;
-    }
-
-    private function progressNotification(callable $process): \Closure
-    {
-        return static function (
-            int $notificationCode,
-            int $severity,
-            string $message,
-            int $messageCode,
-            int $bytesTransferred,
-            int $bytesMax
-        ) use ($process): void {
-            // https://www.php.net/manual/zh/function.stream-notification-callback.php#121236
-            if (STREAM_NOTIFY_PROGRESS === $notificationCode) {
-                $bytesTransferred > 0 and $bytesTransferred += 8192;
-                $process($bytesMax, $bytesTransferred);
-            }
-        };
     }
 
     private function toStreamContext(RequestInterface $request)
     {
         $config = [
             'method' => $request->getMethod(),
-            'header' => $this->toIndexHeaders($request),
+            'header' => self::toIndexHeaders($request),
             'content' => (string) $request->getBody(),
             'protocol_version' => $request->getProtocolVersion(),
         ] + $this->config;
 
         return stream_context_create(['http' => $config], ['notification' => $config['notification']]);
-    }
-
-    private function toIndexHeaders(RequestInterface $request): array
-    {
-        return array_reduce(
-            array_keys($request->getHeaders()),
-            static function (array $headers, string $name) use ($request): array {
-                $values = 'content-length' === strtolower($name)
-                    ? [\strlen((string) $request->getBody())]
-                    : $request->getHeader($name);
-
-                foreach ($values as $value) {
-                    $headers[] = "{$name}: {$value}";
-                }
-
-                return $headers;
-            },
-            []
-        );
     }
 }
