7a0013b4fa98d703fda4c66a9d3bfffd20f5c6d9
yaozm <ityaozm@gmail.com>
2024-03-15 09:48:36 +0800

    feat(deps): Add laravel-http-client-logger and laravel-http-client-global-logger dependencies
    - Added bilfeldt/laravel-http-client-logger version 2.2
    - Added onlime/laravel-http-client-global-logger version 1.1

---
+++ config/http-client-global-logger.php
@@ -0,0 +1,130 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable logging
+    |--------------------------------------------------------------------------
+    |
+    | Whether or not logging should be enabled/disabled.
+    |
+    */
+    'enabled' => env('HTTP_CLIENT_GLOBAL_LOGGER_ENABLED', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mixin variant
+    |--------------------------------------------------------------------------
+    |
+    | Mixin variant enables Http:log($name) feature on Laravel HTTP Client instead
+    | of globally logging everything. This will turn off global logging and only
+    | log requests/responses that have been initiated by this method call.
+    | The only advantage of using this variant is, that you can provide a log
+    | channel name.
+    |
+    */
+    'mixin' => env('HTTP_CLIENT_GLOBAL_LOGGER_MIXIN', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Log to channel
+    |--------------------------------------------------------------------------
+    |
+    | Use the default log channel 'http-client' if you don't want to provide
+    | your own. Logging to 'default' channel is not supported as this channel
+    | needs to use monolog driver.
+    |
+    */
+    'channel' => env('HTTP_CLIENT_GLOBAL_LOGGER_CHANNEL', 'http-client'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Logfile
+    |--------------------------------------------------------------------------
+    |
+    | This is only applied to default log channel 'http-client'.
+    |
+    */
+    'logfile' => env('HTTP_CLIENT_GLOBAL_LOGGER_LOGFILE', storage_path('logs/http-client.log')),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Log message formats
+    |--------------------------------------------------------------------------
+    |
+    | The following variable substitutions are supported:
+    |
+    | - {request}:        Full HTTP request message
+    | - {response}:       Full HTTP response message
+    | - {ts}:             ISO 8601 date in GMT
+    | - {date_iso_8601}   ISO 8601 date in GMT
+    | - {date_common_log} Apache common log date using the configured timezone.
+    | - {host}:           Host of the request
+    | - {method}:         Method of the request
+    | - {uri}:            URI of the request
+    | - {version}:        Protocol version
+    | - {target}:         Request target of the request (path + query + fragment)
+    | - {hostname}:       Hostname of the machine that sent the request
+    | - {code}:           Status code of the response (if available)
+    | - {phrase}:         Reason phrase of the response  (if available)
+    | - {error}:          Any error messages (if available)
+    | - {req_header_*}:   Replace `*` with the lowercased name of a request header to add to the message
+    | - {res_header_*}:   Replace `*` with the lowercased name of a response header to add to the message
+    | - {req_headers}:    Request headers
+    | - {res_headers}:    Response headers
+    | - {req_body}:       Request body
+    | - {res_body}:       Response body
+    |
+    | (see https://github.com/guzzle/guzzle/blob/master/src/MessageFormatter.php)
+    |
+    */
+    'format' => [
+        'request' => env('HTTP_CLIENT_GLOBAL_LOGGER_REQUEST_FORMAT',
+            "REQUEST: {method} {uri}\r\n{req_headers}\r\n{req_body}"
+        ),
+        'response' => env('HTTP_CLIENT_GLOBAL_LOGGER_RESPONSE_FORMAT',
+            "RESPONSE: HTTP/{version} {code} {phrase}\r\n{res_headers}\r\n{res_body}\r\n"
+        ),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Obfuscation of credentials in logs
+    |--------------------------------------------------------------------------
+    |
+    | Obfuscation only works if you have disabled 'mixin' above, which is the default.
+    */
+    'obfuscate' => [
+        'enabled' => env('HTTP_CLIENT_GLOBAL_LOGGER_OBFUSCATE_ENABLED', true),
+        'replacement' => env('HTTP_CLIENT_GLOBAL_LOGGER_OBFUSCATE_REPLACEMENT', '**********'),
+        'headers' => explode(',', env(
+            'HTTP_CLIENT_GLOBAL_LOGGER_OBFUSCATE_HEADERS',
+            'Authorization'
+        )),
+        'body_keys' => explode(',', env(
+            'HTTP_CLIENT_GLOBAL_LOGGER_OBFUSCATE_BODY_KEYS',
+            'pass,password,token,apikey,access_token,refresh_token,client_secret'
+        )),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trim response body
+    |--------------------------------------------------------------------------
+    |
+    | Trim response body to a certain length. This is useful when you are logging
+    | large responses, and you don't want to fill up your log files.
+    |
+    | NOTE the leading comma in trim_response_body.content_type_whitelist default value:
+    | it's there to whitelist empty content types (e.g. when no Content-Type header is set).
+    */
+    'trim_response_body' => [
+        'enabled' => env('HTTP_CLIENT_GLOBAL_LOGGER_TRIM_RESPONSE_BODY_ENABLED', false),
+        'limit' => env('HTTP_CLIENT_GLOBAL_LOGGER_TRIM_RESPONSE_BODY_LIMIT', 200),
+        'content_type_whitelist' => explode(',', env(
+            'HTTP_CLIENT_GLOBAL_LOGGER_TRIM_RESPONSE_BODY_CONTENT_TYPE_WHITELIST',
+            ',application/json'
+        )),
+    ],
+];
---
+++ config/http-client-logger.php
@@ -0,0 +1,87 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Filter class
+    |--------------------------------------------------------------------------
+    |
+    | This is the filter used (unless specified at runtime) to determine if a
+    | request should be logged or not. Any custom filter can be inserted here
+    | but remember that it must implement the HttpLoggingFilterInterface.
+    |
+    */
+    'filter' => \Bilfeldt\LaravelHttpClientLogger\HttpLoggingFilter::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable logging
+    |--------------------------------------------------------------------------
+    |
+    | Whether or not logging should be enabled/disabled when using the filter
+    | specified above. Specifying another filter at runtime will override this
+    | setting.
+    |
+    */
+    'enabled' => env('HTTP_CLIENT_LOGGER_ENABLED', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filtering options
+    |--------------------------------------------------------------------------
+    |
+    | These settings determine what request/responses should be logged. Note
+    | that these settings are only used by the default filter.
+    |
+    */
+    'filter_all' => env('HTTP_CLIENT_LOGGER_FILTER_ALL', false),
+
+    'filter_2xx' => env('HTTP_CLIENT_LOGGER_FILTER_2XX', true),
+
+    'filter_3xx' => env('HTTP_CLIENT_LOGGER_FILTER_3XX', true),
+
+    'filter_4xx' => env('HTTP_CLIENT_LOGGER_FILTER_4XX', true),
+
+    'filter_5xx' => env('HTTP_CLIENT_LOGGER_FILTER_5XX', true),
+
+    'filter_slow' => env('HTTP_CLIENT_LOGGER_FILTER_SLOW', 1.5), // Log requests that took longer than the setting (in sec)
+
+    /*
+    |--------------------------------------------------------------------------
+    | Logger class
+    |--------------------------------------------------------------------------
+    |
+    | This is the logger used (unless specified at runtime) to actually log
+    | the request and response. Any custom logger can be inserted here
+    | but remember that it must implement the HttpLoggerInterface.
+    |
+    */
+    'logger' => \Bilfeldt\LaravelHttpClientLogger\HttpLogger::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Log to channel
+    |--------------------------------------------------------------------------
+    |
+    | These settings determine how to log request/responses to the Laravel log.
+    | Note that these settings are only used by the default logger.
+    | Set to false to disable the channel logging.
+    |
+    */
+    'channel' => env('HTTP_CLIENT_LOGGER_CHANNEL', 'default'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Log to disk
+    |--------------------------------------------------------------------------
+    |
+    | These settings determine how to log request/responses to a flysystem disk.
+    | Note that these settings are only used by the default logger.
+    |
+    */
+    'disk'                => env('HTTP_CLIENT_LOGGER_DISK', false),
+    'disk_separate_files' => true,
+    'prefix_timestamp'    => 'Y-m-d-Hisu', // Leaving empty will remove the timestamp
+    'filename'            => '',
+    'prefix'              => '',
+];
