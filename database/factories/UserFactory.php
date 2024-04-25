f7feb9423d4bf9e369be236788fc717e291bba10
yaozm <ityaozm@gmail.com>
2024-04-25 15:41:34 +0800

    apply rector

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -11,13 +10,6 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = User::class;
-
     /**
      * Define the model's default state.
      *
