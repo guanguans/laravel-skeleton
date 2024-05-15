16831d36b4720752ddf6ffd1758a68a70b88cfce
yaozm <ityaozm@gmail.com>
2024-05-15 14:57:22 +0800

    update config files

--- config/services.php
+++ config/services.php
@@ -29,6 +29,17 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'resend' => [
+        'key' => env('RESEND_KEY'),
+    ],
+
+    'slack' => [
+        'notifications' => [
+            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
+            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+        ],
+    ],
+
     'signer' => [
         'default' => [
             'secret' => env('SIGNER_DEFAULT_SECRET', ''),
