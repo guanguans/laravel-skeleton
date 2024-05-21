74f7bc82db926d0607ac1486cb168f651b3e5325
yaozm <ityaozm@gmail.com>
2024-05-21 10:25:44 +0800

    feat(actions): Add dragon-code/laravel-actions package
    - Added new composer package 'dragon-code/laravel-actions' version 5.0
    - Created new actions/2024_05_21_102443_composer.php file
    - Added actions configuration in config/actions.php file

---
+++ actions/2024_05_21_102443_composer.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use DragonCode\LaravelActions\Action;
+
+return new class extends Action
+{
+    /**
+     * Run the actions.
+     */
+    public function __invoke(): void
+    {
+        //
+    }
+};
