89a87dc742dc299d59593fc60c59acd92a48f171
yaozm <ityaozm@gmail.com>
2024-05-23 11:33:07 +0800

    refactor(guzzle): Improve CircuitBreakerGuzzleMiddleware constructor
    - Added Ganesha namespace for better readability
    - Added missing use statements for Ganesha classes
    - Refactored constructor to accept optional ServiceNameExtractorInterface and FailureDetectorInterface
    - Removed unnecessary properties and initialization in constructor
    - Updated __invoke method to create a new instance of GuzzleMiddleware with injected dependencies

--- app/Support/CircuitBreakerGuzzleMiddleware.php
+++ app/Support/CircuitBreakerGuzzleMiddleware.php
@@ -4,16 +4,27 @@ declare(strict_types=1);
 
 namespace App\Support;
 
+use Ackintosh\Ganesha;
 use Ackintosh\Ganesha\GuzzleMiddleware;
+use Ackintosh\Ganesha\GuzzleMiddleware\FailureDetectorInterface;
+use Ackintosh\Ganesha\GuzzleMiddleware\ServiceNameExtractorInterface;
 use Ackintosh\Ganesha\Storage\Adapter\Redis;
 use Ackintosh\Ganesha\Strategy\Rate\Builder;
+use Illuminate\Support\Traits\Conditionable;
+use Illuminate\Support\Traits\Tappable;
 
 class CircuitBreakerGuzzleMiddleware
 {
-    private readonly GuzzleMiddleware $guzzleMiddleware;
+    use Conditionable;
+    use Tappable;
 
-    public function __construct(array $configuration = [])
-    {
+    private Ganesha $ganesha;
+
+    public function __construct(
+        array $configuration = [],
+        private ?ServiceNameExtractorInterface $serviceNameExtractor = null,
+        private ?FailureDetectorInterface $failureDetector = null
+    ) {
         // 当每 600 秒触发 10 次异常，就会有 90% 的概率的打开断路器，且每 30 秒关闭一次断路器。
         $configuration += [
             'timeWindow' => 600,
@@ -24,23 +35,44 @@ class CircuitBreakerGuzzleMiddleware
             // 'storageKeys' => \Ackintosh\Ganesha\Storage\StorageKeysInterface::class,
         ];
 
-        $ganesha = collect($configuration)
+        $this->ganesha = collect($configuration)
             ->reduce(
                 static fn (
                     Builder $builder,
                     mixed $parameter,
                     string $method
                 ): Builder => $builder->{$method}($parameter),
-                \Ackintosh\Ganesha\Builder::withRateStrategy()
+                Ganesha\Builder::withRateStrategy()
             )
             ?->build();
-
-        $this->guzzleMiddleware = new GuzzleMiddleware($ganesha);
     }
 
     public function __invoke(callable $handler): \Closure
     {
         /** @throws \Ackintosh\Ganesha\Exception\RejectedException */
-        return ($this->guzzleMiddleware)($handler);
+        return (new GuzzleMiddleware(
+            $this->ganesha,
+            $this->serviceNameExtractor,
+            $this->failureDetector
+        ))($handler);
+    }
+
+    public function getGanesha(): Ganesha
+    {
+        return $this->ganesha;
+    }
+
+    public function setServiceNameExtractor(?ServiceNameExtractorInterface $serviceNameExtractor): self
+    {
+        $this->serviceNameExtractor = $serviceNameExtractor;
+
+        return $this;
+    }
+
+    public function setFailureDetector(?FailureDetectorInterface $failureDetector): self
+    {
+        $this->failureDetector = $failureDetector;
+
+        return $this;
     }
 }
