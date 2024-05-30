5542d6a05697fc49af5a3016413218327903046f
yaozm <ityaozm@gmail.com>
2024-05-30 14:56:02 +0800

    refactor(HealthCheckCommand): Refactor handle method for improved readability and maintainability
    - Refactor handle method in HealthCheckCommand for improved readability and maintainability
    - Use explicit type declarations for private properties
    - Use str() instead of Str::of() for string manipulation
    - Add missing type declarations for method parameters
    - Improve error handling and messaging in the handle method

--- app/Console/Commands/HealthCheckCommand.php
+++ app/Console/Commands/HealthCheckCommand.php
@@ -1,5 +1,7 @@
 <?php
 
+/** @noinspection PhpUnusedPrivateMethodInspection */
+
 namespace App\Console\Commands;
 
 use App\Enums\HealthCheckStateEnum;
@@ -10,9 +12,10 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Queue;
-use Illuminate\Support\Str;
 use ReflectionMethod;
 use ReflectionObject;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Throwable;
 
 class HealthCheckCommand extends Command
@@ -22,7 +25,10 @@ class HealthCheckCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'health:check';
+    protected $signature = 'health:check
+        {--only=* : Only check methods with the given name}
+        {--except=* : Do not check methods with the given name}
+    ';
 
     /**
      * The console command description.
@@ -31,81 +37,78 @@ class HealthCheckCommand extends Command
      */
     protected $description = 'Health check.';
 
-    /**
-     * @var array
-     */
-    protected $except = [
-        '*Queue',
-    ];
+    private array $only = [];
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    private array $except = [];
 
     /**
      * Execute the console command.
      */
-    public function handle(): int
+    public function handle(): void
     {
-        collect((new ReflectionObject($this))->getMethods(ReflectionMethod::IS_PROTECTED | ReflectionMethod::IS_PRIVATE))
-            ->filter(static fn (ReflectionMethod $method) => Str::of($method->name)->startsWith('check'))
-            ->reject(fn (ReflectionMethod $method) => Str::of($method->name)->is($this->except))
+        collect((new ReflectionObject($this))->getMethods(ReflectionMethod::IS_PRIVATE))
+            ->filter(static fn (ReflectionMethod $method) => str($method->name)->startsWith('check'))
+            ->when($this->only, fn (Collection $methods) => $methods->filter(
+                fn (ReflectionMethod $method) => str($method->name)->is($this->only)
+            ))
+            ->when($this->except, fn (Collection $methods) => $methods->reject(
+                fn (ReflectionMethod $method) => str($method->name)->is($this->except)
+            ))
             ->sortBy(static fn (ReflectionMethod $method) => $method->name)
             ->pipe(function (Collection $methods) {
-                $this->withProgressBar($methods, function ($method) use (&$checks): void {
-                    /** @var HealthCheckStateEnum $state */
-                    $state = \call_user_func([$this, $method->name]);
+                $this
+                    ->setProcessTitle('Health checking...')
+                    ->withProgressBar($methods, function ($method) use (&$checks): void {
+                        $state = $this->{$method->name}();
 
-                    $checks[] = [
-                        'index' => \count((array) $checks) + 1,
-                        'resource' => Str::of($method->name)->replaceFirst('check', ''),
-                        'state' => $state,
-                        'message' => $state->description,
-                    ];
-                });
+                        \assert($state instanceof HealthCheckStateEnum);
 
-                $this->newLine();
-                $this->table(['Index', 'Resource', 'State', 'Message'], $checks);
+                        $checks[] = [
+                            'index' => \count((array) $checks) + 1,
+                            'resource' => str($method->name)->replaceFirst('check', ''),
+                            'state' => $state,
+                            'message' => $state->description,
+                        ];
+                    });
 
                 return collect($checks);
             })
+            ->tap(fn (Collection $checks) => $this->newLine()->table(
+                ['Index', 'Resource', 'State', 'Message'],
+                $checks->all()
+            ))
             ->filter(static fn ($check): bool => $check['state']->isNot(HealthCheckStateEnum::OK))
-            ->whenNotEmpty(function (Collection $notOkChecks) {
-                // event(new HealthCheckFailedEvent($notOkChecks));
-                $this->error('Health check failed.');
-
-                return $notOkChecks;
+            ->whenNotEmpty(function (): void {
+                $this->components->error('Health check failed.');
             })
-            ->whenEmpty(function (Collection $notOkChecks) {
-                // event(new HealthCheckPassedEvent());
-                $this->info('Health check passed.');
-
-                return $notOkChecks;
+            ->whenEmpty(function (): void {
+                $this->components->info('Health check passed.');
             });
-
-        return self::SUCCESS;
     }
 
-    protected function checkDatabase($connection = null): HealthCheckStateEnum
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        $this->only = array_merge($this->only, $this->option('only'));
+        $this->except = array_merge($this->except, $this->option('except'));
+    }
+
+    private function checkDatabase(?string $connection = null): HealthCheckStateEnum
     {
         try {
             DB::connection($connection ?: config('database.default'))->getPdo();
         } catch (Throwable $throwable) {
-            return tap(HealthCheckStateEnum::FAILING(), static function (HealthCheckStateEnum $state) use ($throwable): void {
-                $state->description = "Could not connect to the database: `{$throwable->getMessage()}`";
-            });
+            return tap(
+                HealthCheckStateEnum::FAILING(),
+                static function (HealthCheckStateEnum $state) use ($throwable): void {
+                    $state->description = "Could not connect to the database: `{$throwable->getMessage()}`";
+                }
+            );
         }
 
         return HealthCheckStateEnum::OK();
     }
 
-    protected function checkSqlSafeUpdates(): HealthCheckStateEnum
+    private function checkSqlSafeUpdates(): HealthCheckStateEnum
     {
         if (config('database.default') !== 'mysql') {
             return tap(HealthCheckStateEnum::WARNING(), static function (HealthCheckStateEnum $state): void {
@@ -114,7 +117,7 @@ class HealthCheckCommand extends Command
         }
 
         $sqlSafeUpdates = DB::select("SHOW VARIABLES LIKE 'sql_safe_updates' ")[0];
-        if (! Str::of($sqlSafeUpdates->Value)->lower()->is('on')) {
+        if (! str($sqlSafeUpdates->Value)->lower()->is('on')) {
             return tap(HealthCheckStateEnum::FAILING(), static function (HealthCheckStateEnum $state): void {
                 $state->description = '`sql_safe_updates` is disabled. Please enable it.';
             });
@@ -124,9 +127,9 @@ class HealthCheckCommand extends Command
     }
 
     /**
-     * @param  array|string  $checkedSqlModes
+     * @param  array<string>|string  $checkedSqlModes
      */
-    protected function checkSqlMode($checkedSqlModes = 'strict_all_tables'): HealthCheckStateEnum
+    private function checkSqlMode(array|string $checkedSqlModes = 'strict_all_tables'): HealthCheckStateEnum
     {
         if (config('database.default') !== 'mysql') {
             return tap(HealthCheckStateEnum::WARNING(), static function (HealthCheckStateEnum $state): void {
@@ -136,17 +139,24 @@ class HealthCheckCommand extends Command
 
         $sqlModes = DB::select("SHOW VARIABLES LIKE 'sql_mode' ")[0];
 
-        /** @var Collection $diffSqlModes */
-        $diffSqlModes = Str::of($sqlModes->Value)
+        $diffSqlModes = str($sqlModes->Value)
             ->lower()
             ->explode(',')
-            ->pipe(static fn (Collection $sqlModes): Collection => collect($checkedSqlModes)
-                ->transform(static fn (string $checkedSqlMode) => Str::of($checkedSqlMode)->lower())
-                ->diff($sqlModes));
+            ->pipe(
+                static fn (Collection $sqlModes): Collection => collect($checkedSqlModes)
+                    ->transform(static fn (string $checkedSqlMode) => str($checkedSqlMode)->lower())
+                    ->diff($sqlModes)
+            );
+
+        \assert($diffSqlModes instanceof Collection);
+
         if ($diffSqlModes->isNotEmpty()) {
-            return tap(HealthCheckStateEnum::FAILING(), static function (HealthCheckStateEnum $state) use ($diffSqlModes): void {
-                $state->description = "`sql_mode` is not set to `{$diffSqlModes->implode('、')}`. Please set to them.";
-            });
+            return tap(
+                HealthCheckStateEnum::FAILING(),
+                static function (HealthCheckStateEnum $state) use ($diffSqlModes): void {
+                    $state->description = "`sql_mode` is not set to `{$diffSqlModes->implode('、')}`. Please set to them.";
+                }
+            );
         }
 
         return HealthCheckStateEnum::OK();
@@ -155,7 +165,7 @@ class HealthCheckCommand extends Command
     /**
      * @throws \Exception
      */
-    protected function checkTimeZone(): HealthCheckStateEnum
+    private function checkTimeZone(): HealthCheckStateEnum
     {
         if (config('database.default') !== 'mysql') {
             return tap(HealthCheckStateEnum::WARNING(), static function (HealthCheckStateEnum $state): void {
@@ -164,34 +174,41 @@ class HealthCheckCommand extends Command
         }
 
         $dbTimeZone = DB::select("SHOW VARIABLES LIKE 'time_zone' ")[0]->Value;
-        Str::of($dbTimeZone)->lower()->is('system') and $dbTimeZone = DB::select("SHOW VARIABLES LIKE 'system_time_zone' ")[0]->Value;
+        if (str($dbTimeZone)->lower()->is('system')) {
+            $dbTimeZone = DB::select("SHOW VARIABLES LIKE 'system_time_zone' ")[0]->Value;
+        }
 
         $dbDateTime = (new DateTime('now', new DateTimeZone($dbTimeZone)))->format('YmdH');
         $appDateTime = (new DateTime('now', new DateTimeZone($appTimezone = config('app.timezone'))))->format('YmdH');
         if ($dbDateTime !== $appDateTime) {
-            return tap(HealthCheckStateEnum::FAILING(), static function (HealthCheckStateEnum $state) use ($appTimezone, $dbTimeZone): void {
-                $state->description = "The database timezone(`$dbTimeZone`) is not equal to app timezone(`$appTimezone`).";
-            });
+            return tap(
+                HealthCheckStateEnum::FAILING(),
+                static function (HealthCheckStateEnum $state) use ($dbTimeZone, $appTimezone): void {
+                    $state->description = "The database timezone(`$dbTimeZone`) is not equal to app timezone(`$appTimezone`).";
+                }
+            );
         }
 
         return HealthCheckStateEnum::OK();
     }
 
-    protected function checkPing(?string $url = null): HealthCheckStateEnum
+    private function checkPing(?string $url = null): HealthCheckStateEnum
     {
-        $url = $url ?: config('app.url');
-
-        $response = Http::get($url);
+        $response = Http::get($url ?: config('app.url'));
         if ($response->failed()) {
-            return tap(HealthCheckStateEnum::FAILING(), static function (HealthCheckStateEnum $state) use ($response): void {
-                $state->description = "Could not connect to the application: `{$response->body()}`";
-            });
+            return tap(
+                HealthCheckStateEnum::FAILING(),
+                static function (HealthCheckStateEnum $state) use ($response): void {
+                    // $state->description = "Could not connect to the application: `{$response->body()}`";
+                    $state->description = "Could not connect to the application: `{$response->reason()}`";
+                }
+            );
         }
 
         return HealthCheckStateEnum::OK();
     }
 
-    protected function checkPhpVersion(): HealthCheckStateEnum
+    private function checkPhpVersion(): HealthCheckStateEnum
     {
         if (PHP_VERSION_ID < 80100) {
             return tap(HealthCheckStateEnum::FAILING(), static function (HealthCheckStateEnum $state): void {
@@ -202,9 +219,9 @@ class HealthCheckCommand extends Command
         return HealthCheckStateEnum::OK();
     }
 
-    protected function checkPhpExtensions(): HealthCheckStateEnum
+    private function checkPhpExtensions(): HealthCheckStateEnum
     {
-        $extensions = [
+        $missingExtensions = collect([
             'curl',
             'gd',
             'mbstring',
@@ -214,45 +231,57 @@ class HealthCheckCommand extends Command
             'xml',
             'zip',
             'swoole',
-        ];
+        ])->reduce(
+            static fn (Collection $missingExtensions, $extension) => $missingExtensions->when(
+                ! \extension_loaded($extension),
+                static fn (Collection $missingExtensions) => $missingExtensions->add($extension)
+            ),
+            collect()
+        );
 
-        /** @var Collection $missingExtensions */
-        $missingExtensions = collect($extensions)
-            ->reduce(static fn (Collection $missingExtensions, $extension) => $missingExtensions->when(! \extension_loaded($extension), static fn (Collection $missingExtensions) => $missingExtensions->add($extension)), collect());
+        \assert($missingExtensions instanceof Collection);
 
         if ($missingExtensions->isNotEmpty()) {
-            return tap(HealthCheckStateEnum::FAILING(), static function (HealthCheckStateEnum $state) use ($missingExtensions): void {
-                $state->description = "The following PHP extensions are missing: `{$missingExtensions->implode('、')}`.";
-            });
+            return tap(
+                HealthCheckStateEnum::FAILING(),
+                static function (HealthCheckStateEnum $state) use ($missingExtensions): void {
+                    $state->description = "The following PHP extensions are missing: `{$missingExtensions->implode('、')}`.";
+                }
+            );
         }
 
         return HealthCheckStateEnum::OK();
     }
 
-    protected function checkDiskSpace(): HealthCheckStateEnum
+    private function checkDiskSpace(): HealthCheckStateEnum
     {
         $freeSpace = disk_free_space(base_path());
         $diskSpace = sprintf('%.1f', $freeSpace / (1024 * 1024));
         if ($diskSpace < 100) {
-            return tap(HealthCheckStateEnum::FAILING(), static function (HealthCheckStateEnum $state) use ($diskSpace): void {
-                $state->description = "The disk space is less than 100MB: `$diskSpace`.";
-            });
+            return tap(
+                HealthCheckStateEnum::FAILING(),
+                static function (HealthCheckStateEnum $state) use ($diskSpace): void {
+                    $state->description = "The disk space is less than 100MB: `$diskSpace`.";
+                }
+            );
         }
 
         $diskSpace = sprintf('%.1f', $freeSpace / (1024 * 1024 * 1024));
         if ($diskSpace < 1) {
-            return tap(HealthCheckStateEnum::WARNING(), static function (HealthCheckStateEnum $state) use ($diskSpace): void {
-                $state->description = "The disk space is less than 1GB: `$diskSpace`.";
-            });
+            return tap(
+                HealthCheckStateEnum::WARNING(),
+                static function (HealthCheckStateEnum $state) use ($diskSpace): void {
+                    $state->description = "The disk space is less than 1GB: `$diskSpace`.";
+                }
+            );
         }
 
         return HealthCheckStateEnum::OK();
     }
 
-    protected function checkMemoryLimit(int $limit = 128): HealthCheckStateEnum
+    private function checkMemoryLimit(int $limit = 128): HealthCheckStateEnum
     {
         $inis = collect(ini_get_all())->filter(static fn ($value, $key): bool => str_contains($key, 'memory_limit'));
-
         if ($inis->isEmpty()) {
             return tap(HealthCheckStateEnum::FAILING(), static function (HealthCheckStateEnum $state): void {
                 $state->description = 'The memory limit is not set.';
@@ -261,15 +290,18 @@ class HealthCheckCommand extends Command
 
         $localValue = $inis->first()['local_value'];
         if ($localValue < $limit) {
-            return tap(HealthCheckStateEnum::FAILING(), static function (HealthCheckStateEnum $state) use ($limit, $localValue): void {
-                $state->description = "The memory limit is less than {$limit}M: `$localValue`.";
-            });
+            return tap(
+                HealthCheckStateEnum::FAILING(),
+                static function (HealthCheckStateEnum $state) use ($limit, $localValue): void {
+                    $state->description = "The memory limit is less than {$limit}M: `$localValue`.";
+                }
+            );
         }
 
         return HealthCheckStateEnum::OK();
     }
 
-    protected function checkQueue(): HealthCheckStateEnum
+    private function checkQueue(): HealthCheckStateEnum
     {
         if (! Queue::connected()) {
             return tap(HealthCheckStateEnum::FAILING(), static function (HealthCheckStateEnum $state): void {
