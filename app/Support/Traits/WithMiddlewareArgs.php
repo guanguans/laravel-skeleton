03ad4014002fb348622fb60272570b49170f7e67
yaozm <ityaozm@gmail.com>
2024-06-05 10:56:52 +0800

    feat(WithMiddlewareArgs): Add WithMiddlewareArgs trait
    - Added WithMiddlewareArgs trait to handle middleware arguments
    - Implemented static method `with` to handle arguments and return a formatted string

---
+++ app/Support/Traits/WithMiddlewareArgs.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/laravel-skeleton.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace App\Support\Traits;
+
+trait WithMiddlewareArgs
+{
+    public static function with(...$args): string
+    {
+        if ($args === []) {
+            return static::class;
+        }
+
+        return static::class.':'.implode(',', $args);
+    }
+}
