df3e3ec1849ba0881e76b5d20749054416b472a7
yaozm <ityaozm@gmail.com>
2024-07-19 21:51:07 +0800

    refactor(MaxUploadSizeRule): remove macro usage and improve logic
    Replace the macro approach with direct code implementation in the `passes` method
    to determine the maximum upload size based on php.ini settings. This change
    simplifies the rule by removing unnecessary abstractions and enhances the
    codebase maintainability.

--- app/Rules/MaxUploadSizeRule.php
+++ app/Rules/MaxUploadSizeRule.php
@@ -2,60 +2,44 @@
 
 namespace App\Rules;
 
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rules\File;
 
 /**
  * @see https://github.com/antoninmasek/laravel-max-upload-size-rule
  * @see https://tonymasek.com/blog/easily-validate-max-file-size-based-on-your-settings-in-phpini
- *
- * @todo
  */
 final class MaxUploadSizeRule extends Rule
 {
-    public function __construct()
-    {
-        /**
-         * This method is here in order to mock calls to `ini_get` when testing.
-         */
-        App::macro('getPhpIniValue', function (string $key): false|string {
-            return \ini_get($key);
-        });
-
-        File::macro('maxUploadSize', function (): \Illuminate\Contracts\Validation\Rule {
-            $uploadMaxSizeInBytes = ini_parse_quantity(App::getPhpIniValue('upload_max_filesize'));
-            if ($uploadMaxSizeInBytes > 0) {
-                return $this->max($uploadMaxSizeInBytes / 1024);
-            }
-
-            $postMaxSizeInBytes = ini_parse_quantity(App::getPhpIniValue('post_max_size'));
-            if ($postMaxSizeInBytes > 0) {
-                return $this->max($postMaxSizeInBytes / 1024);
-            }
-
-            return $this;
-        });
-    }
-
     public function passes(string $attribute, mixed $value): bool
     {
-        return Hash::check($value, auth()->user()?->getAuthPassword());
+        if (($maxUploadSizeInBytes = $this->maxUploadSize()) < 0) {
+            return true;
+        }
+
+        return Validator::make(
+            [$attribute => $value],
+            [$attribute => \Illuminate\Validation\Rule::file()->max($maxUploadSizeInBytes / 1024)]
+        )->passes();
     }
 
-    public function validate(string $attribute, mixed $value, \Closure $fail): void
+    private function maxUploadSize(): int
     {
-        $rule = \Illuminate\Validation\Rule::file()->maxUploadSize();
-
-        $validator = Validator::make(
-            [$attribute => $value],
-            [$attribute => $rule]
-        );
-
-        if ($validator->fails()) {
-            $fail(Arr::first($rule->message()));
+        // We know, that `upload_max_filesize` has to have a lower value
+        // than `post_max_size`. So in case there is a limit set
+        // we should use this because it can't be higher than `post_max_size`
+        $uploadMaxSize = ini_parse_quantity(\ini_get('upload_max_filesize'));
+        if ($uploadMaxSize > 0) {
+            return $uploadMaxSize;
         }
+
+        // In case `upload_max_filesize` is 0 or lower, it means, that
+        // the limit is disabled. In that case, check for the value of `post_max_size`
+        $postMaxSize = ini_parse_quantity(\ini_get('post_max_size'));
+        if ($postMaxSize > 0) {
+            return $postMaxSize;
+        }
+
+        // Limit is disabled
+        return -1;
     }
 }
