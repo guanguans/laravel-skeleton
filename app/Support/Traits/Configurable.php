00d949d919d66125d93924ac153d793f503e43b2
guanguans <ityaozm@gmail.com>
2024-08-15 09:53:57 +0800

    feat(trait): add Configurable trait for option management
    - Introduced a new trait `Configurable` for managing configuration options.
    - Added `configure` method to set options using a closure.
    - Implemented `configureOptions` method to resolve options with Symfony's `OptionsResolver`.
    - This enhances flexibility in configuring options across classes.

---
+++ app/Support/Traits/Configurable.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/laravel-skeleton.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace App\Support\Traits;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+trait Configurable
+{
+    protected array $options;
+
+    public function configure(array $options, \Closure $closure): self
+    {
+        $this->options = $this->configureOptions($options, $closure);
+
+        return $this;
+    }
+
+    public function configureOptions(array $options, \Closure $closure): array
+    {
+        $resolver = new OptionsResolver;
+
+        $closure($resolver);
+
+        return $resolver->resolve($options);
+    }
+}
