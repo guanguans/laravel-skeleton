24fdd2c100c9300c0133882ba8c72c6323becb5f
guanguans <ityaozm@gmail.com>
2024-08-20 15:24:31 +0800

    test(ArchTest): add initial test for debugging function usage
    - Create a new test file ArchTest.php
    - Set strict types for better type safety
    - Add commented-out test for ensuring debugging functions are not used
    - List debugging functions to be checked against

---
+++ tests/ArchTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+// arch('will not use debugging functions')
+//     ->expect([
+//         'dd',
+//         'die',
+//         'dump',
+//         'echo',
+//         'exit',
+//         'print',
+//         'print_r',
+//         'printf',
+//         'ray',
+//         'trap',
+//         'var_dump',
+//         'var_export',
+//         'vprintf',
+//     ])
+//     ->each->not->toBeUsed();
