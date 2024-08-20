bee626d02abcf93305424bfaf0cb2a330990507e
guanguans <ityaozm@gmail.com>
2024-08-20 14:54:26 +0800

    feat(middleware): add MustBeAdmin middleware and isAdmin method
    - Create a new middleware `MustBeAdmin` to restrict access to admin users.
    - Implement the `handle` method to check if the user is authenticated and an admin, returning a 403 error for unauthorized access.
    - Add a placeholder `isAdmin` method in the `User` model for future implementation.

---
+++ app/Http/Middleware/MustBeAdmin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class MustBeAdmin
+{
+    public function handle(Request $request, Closure $next)
+    {
+        abort_if(auth()->guest() || ! $request->user()->isAdmin(), 403, '非法访问！');
+
+        return $next($request);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -80,4 +80,9 @@ class User extends Authenticatable
     {
         return $this->morphMany(DatabaseNotification::class, 'notifiable')->latest();
     }
+
+    public function isAdmin(): bool
+    {
+        return false; // todo implement
+    }
 }
