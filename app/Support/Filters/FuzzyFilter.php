4262c98e1dbc93e8883a725e65bd3468b2ef13cb
guanguans <ityaozm@gmail.com>
2024-09-29 09:56:30 +0800

    feat(filters): add FuzzyFilter class for flexible querying
    - Introduce FuzzyFilter class to enable fuzzy searching in queries.
    - Accepts multiple fields for searching using the LIKE operator.
    - Implements the Filter interface for compatibility with Spatie QueryBuilder.
    - Enhances the querying capabilities of the application.

---
+++ app/Support/Filters/FuzzyFilter.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Support\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\QueryBuilder\Filters\Filter;
+
+/**
+ * @template TModelClass of Model
+ *
+ * @implements Filter<TModelClass>
+ *
+ * @see https://github.com/wayofdev/laravel-starter-tpl/blob/develop/app/src/Infrastructure/Filters/FuzzyFilter.php
+ */
+class FuzzyFilter implements Filter
+{
+    /**
+     * @var string[]
+     */
+    private array $fields;
+
+    public function __construct(string ...$fields)
+    {
+        $this->fields = $fields;
+    }
+
+    /**
+     * @param  Builder<TModelClass>  $query
+     * @return Builder<TModelClass>
+     */
+    public function __invoke(Builder $query, mixed $value, string $property): Builder
+    {
+        $query->where(function (Builder $query) use ($value): void {
+            foreach ($this->fields as $field) {
+                $values = (array) $value;
+
+                foreach ($values as $item) {
+                    $query->orWhere($field, 'LIKE', "%$item%");
+                }
+            }
+        });
+
+        return $query;
+    }
+}
