e0a5fd8803fc08610f48576e46a4a5479748d347
guanguans <ityaozm@gmail.com>
2024-10-14 15:39:25 +0800

    chore(service-provider): Update service providers and composer.json
    - Added new service providers for IDE helper and API debugger.
    - Updated the service provider for Laravel Soar to be uncommented.
    - Adjusted the 'dont-discover' section in composer.json to include additional packages.
    - Added a new entry for ApiResponse in phpstan-baseline.neon.

--- app/Providers/UnlessProductionServiceProvider.php
+++ app/Providers/UnlessProductionServiceProvider.php
@@ -11,10 +11,12 @@ class UnlessProductionServiceProvider extends AggregateServiceProvider
      */
     protected $providers = [
         \AndreasElia\PostmanGenerator\PostmanGeneratorServiceProvider::class,
+        \Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         \ChrisDiCarlo\LaravelConfigChecker\LaravelConfigCheckerServiceProvider::class,
         \Enlightn\Enlightn\EnlightnServiceProvider::class,
-        // \Guanguans\LaravelSoar\SoarServiceProvider::class,
+        \Guanguans\LaravelSoar\SoarServiceProvider::class,
         \KitLoong\MigrationsGenerator\MigrationsGeneratorServiceProvider::class,
+        \Lanin\Laravel\ApiDebugger\ServiceProvider::class,
         \LaraDumps\LaraDumps\LaraDumpsServiceProvider::class,
         \Laracademy\Generators\GeneratorsServiceProvider::class,
         \LaracraftTech\LaravelSchemaRules\LaravelSchemaRulesServiceProvider::class,
@@ -23,6 +25,7 @@ class UnlessProductionServiceProvider extends AggregateServiceProvider
         \Laravel\Telescope\TelescopeServiceProvider::class,
         \Mortexa\LaravelArkitect\ArkitectServiceProvider::class,
         \Msamgan\LaravelEnvKeysChecker\LaravelEnvKeysCheckerServiceProvider::class,
+        \NunoMaduro\Collision\Adapters\Laravel\CollisionServiceProvider::class,
         \Orangehill\Iseed\IseedServiceProvider::class,
         \PrettyRoutes\ServiceProvider::class,
         \Reliese\Coders\CodersServiceProvider::class,
