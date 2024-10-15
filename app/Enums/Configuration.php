1d2b97eccf3b1813493b9e65667c940f0914032a
guanguans <ityaozm@gmail.com>
2024-10-15 10:23:10 +0800

    feat(enums): add Configuration enum for scheduling options
    - Introduce a new enum called Configuration to define scheduling options.
    - Enum cases include DAILY, WEEKLY, and MONTHLY.
    - Implement tryFromValue method to retrieve enum case from string value.
    - Enhance type safety and improve code readability.

---
+++ app/Enums/Configuration.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+use Cerbero\Enum\Concerns\Enumerates;
+use Elao\Enum\ExtrasTrait;
+use Elao\Enum\ReadableEnumTrait;
+use EmreYarligan\EnumConcern\EnumConcern;
+
+/**
+ * @see https://masteringlaravel.io/daily/2024-10-14-a-use-case-for-the-value-of-phpdoc-type
+ */
+enum Configuration: string
+{
+    // use EnumConcern;
+    use Enumerates;
+    use ExtrasTrait;
+    use ReadableEnumTrait;
+
+
+    case DAILY = 'daily';
+    case WEEKLY = 'weekly';
+    case MONTHLY = 'monthly';
+
+
+    /**
+     * @param  value-of<self>  $value
+     */
+    public static function tryFromValue(string $value): ?self
+    {
+        foreach (self::cases() as $case) {
+            if ($case->value === $value) {
+                return $case;
+            }
+        }
+
+        return null;
+    }
+}
