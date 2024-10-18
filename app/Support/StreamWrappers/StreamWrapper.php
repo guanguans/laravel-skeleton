2719d8fbc2cea35cd692a7520cd2e15ab44b9839
guanguans <ityaozm@gmail.com>
2024-10-18 21:20:35 +0800

    docs(streamwrapper): Add documentation for stream wrapper methods
    - Added documentation for the constructor method
    - Added documentation for the destructor method
    - Added documentation for the dir_closedir method
    - Added        documentation for the dir_opendir method
    - Added documentation for the dir_readdir method
    - Added documentation for the dir_rewinddir method
    - Added documentation for the  mkdir method
    - Added documentation for the rename method
    - Added documentation for the rmdir method
    - Added documentation for the stream_cast method
    - Added documentation  for the stream_close method
    - Added documentation for the stream_eof method
    - Added documentation for the stream_flush method
    - Added documentation for the stream_lock    method
    - Added documentation for the stream_metadata method
    - Added documentation for the stream_open method
    - Added documentation for the stream_read method
    - Added     documentation for the stream_seek method
    - Added documentation for the stream_set_option method
    - Added documentation for the stream_stat method
    - Added documentation for  the stream_tell method
    - Added documentation for the stream_truncate method
    - Added documentation for the stream_write method
    - Added documentation for the unlink method
    -  Added documentation for the url_stat method

--- app/Support/StreamWrappers/StreamWrapper.php
+++ app/Support/StreamWrappers/StreamWrapper.php
@@ -38,44 +38,75 @@ abstract class StreamWrapper
         return \in_array(static::name(), stream_get_wrappers(), true);
     }
 
+    /**
+     * Constructs a new stream wrapper
+     */
     public function __construct() {}
 
+    /**
+     * Destructs an existing stream wrapper
+     */
+    public function __destruct() {}
+
+    /**
+     * Close directory handle
+     */
     public function dir_closedir(): bool
     {
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
+    /**
+     * Open directory handle
+     */
     public function dir_opendir(string $path, int $options): bool
     {
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
+    /**
+     * Read entry from directory handle
+     */
     public function dir_readdir(): string
     {
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
+    /**
+     * Rewind directory handle
+     */
     public function dir_rewinddir(): bool
     {
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
+    /**
+     * Create a directory
+     */
     public function mkdir(string $path, int $mode, int $options): bool
     {
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
+    /**
+     * Renames a file or directory
+     */
     public function rename(string $path_from, string $path_to): bool
     {
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
+    /**
+     * Removes a directory
+     */
     public function rmdir(string $path, int $options): bool
     {
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
     /**
+     * Retrieve the underlaying resource
+     *
      * @return resource
      */
     public function stream_cast(int $cast_as): mixed
@@ -84,11 +115,15 @@ abstract class StreamWrapper
     }
 
     /**
+     * Close a resource
+     *
      * @see \fclose()
      */
     public function stream_close(): void {}
 
     /**
+     * Tests for end-of-file on a file pointer
+     *
      * @see \feof()
      */
     public function stream_eof(): bool
@@ -97,6 +132,8 @@ abstract class StreamWrapper
     }
 
     /**
+     * Flushes the output
+     *
      * @see \fflush()
      */
     public function stream_flush(): bool
@@ -105,6 +142,8 @@ abstract class StreamWrapper
     }
 
     /**
+     * Advisory file locking
+     *
      * @see \flock()
      */
     public function stream_lock(int $operation): bool
@@ -112,12 +151,17 @@ abstract class StreamWrapper
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
+    /**
+     * Change stream metadata
+     */
     public function stream_metadata(string $path, int $option, mixed $value): bool
     {
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
     /**
+     * Opens file or URL
+     *
      * @see \fopen()
      */
     public function stream_open(string $path, string $mode, int $options, ?string &$opened_path): bool
@@ -126,6 +170,8 @@ abstract class StreamWrapper
     }
 
     /**
+     * Read from stream
+     *
      * @see \fread()
      */
     public function stream_read(int $count): string|false
@@ -134,6 +180,8 @@ abstract class StreamWrapper
     }
 
     /**
+     * Seeks to specific location in a stream
+     *
      * @see \fseek()
      */
     public function stream_seek(int $offset, int $whence = SEEK_SET): bool
@@ -141,12 +189,17 @@ abstract class StreamWrapper
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
+    /**
+     * Change stream options
+     */
     public function stream_set_option(int $option, int $arg1, int $arg2): bool
     {
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
     /**
+     * Retrieve information about a file resource
+     *
      * @see \fstat()
      */
     public function stream_stat(): array|false
@@ -155,6 +208,8 @@ abstract class StreamWrapper
     }
 
     /**
+     * Retrieve the current position of a stream
+     *
      * @see \ftell()
      */
     public function stream_tell(): int
@@ -163,6 +218,8 @@ abstract class StreamWrapper
     }
 
     /**
+     * Truncate stream
+     *
      * @see \ftruncate()
      */
     public function stream_truncate(int $new_size): bool
@@ -171,6 +228,8 @@ abstract class StreamWrapper
     }
 
     /**
+     * Write to stream
+     *
      * @see \fwrite()
      */
     public function stream_write(string $data): int
@@ -178,17 +237,21 @@ abstract class StreamWrapper
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
+    /**
+     * Delete a file
+     */
     public function unlink(string $path): bool
     {
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
 
+    /**
+     * Retrieve information about a file
+     */
     public function url_stat(string $path, int $flags): array|false
     {
         $this->throwMethodNotImplemented(__FUNCTION__);
     }
-
-    public function __destruct() {}
 
     /**
      * @throws \BadMethodCallException
