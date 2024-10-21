2f061378409430a88d0ff4d54fd693e538d01200
guanguans <ityaozm@gmail.com>
2024-10-21 16:45:21 +0800

    feat(PhpUserFilters): Add CallbackFilter class
    - Added CallbackFilter class to the `App\Support\PhpUserFilters` namespace.
    - The CallbackFilter class extends the `\Clue\StreamFilter\CallbackFilter` class.
    -  The CallbackFilter class is used as a PHP user filter.
    - The source file is licensed under the MIT license.
    - More information about the `php-user-filter` class can be found  at the provided link.

---
+++ app/Support/PhpUserFilters/CallbackFilter.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/laravel-skeleton.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace App\Support\PhpUserFilters;
+
+/**
+ * @see https://www.php.net/manual/zh/book.stream.php
+ * @see https://www.php.net/manual/zh/class.php-user-filter.php
+ */
+class CallbackFilter extends \Clue\StreamFilter\CallbackFilter {}
