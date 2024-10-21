4127e0fd2c7ff51cca8d5c74bc4efd3c570fc741
guanguans <ityaozm@gmail.com>
2024-10-21 10:00:11 +0800

    feat(app): add JSON rendering for API requests
    - Added logic to check if the request is for an API endpoint
    - If the request is for an API endpoint, render the response as JSON
    - Also render the response as JSON  if the request expects JSON

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,4 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         // $exceptions->dontFlash([]);
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+            if ($request->is('api/*')) {
+                return true;
+            }
+
+            return $request->expectsJson();
+        });
     })->create();
