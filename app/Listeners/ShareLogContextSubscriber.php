23c28c7f3c62f5ce937f3567022a03bb3dd64fe9
guanguans <ityaozm@gmail.com>
2024-10-22 15:15:10 +0800

    feat(listeners): Add logging context for RequestHandled and CommandFinished events
    - Import the necessary event classes
    - Add event listeners for RequestHandled and CommandFinished events
    - Share memory usage context in the event listeners

--- app/Listeners/ShareLogContextSubscriber.php
+++ app/Listeners/ShareLogContextSubscriber.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Listeners;
 
+use Illuminate\Console\Events\CommandFinished;
 use Illuminate\Console\Events\CommandStarting;
 use Illuminate\Console\Events\ScheduledTaskStarting;
 use Illuminate\Events\Dispatcher;
+use Illuminate\Foundation\Http\Events\RequestHandled;
 use Illuminate\Routing\Events\RouteMatched;
 use Illuminate\Support\Facades\Log;
 
@@ -28,6 +30,18 @@ class ShareLogContextSubscriber
             ScheduledTaskStarting::class => static function (ScheduledTaskStarting $event): void {
                 Log::shareContext([
                     'command' => ($event->task->command ?: $event->task->description) ?: 'Closure',
+                ]);
+            },
+            RequestHandled::class => static function (RequestHandled $event): void {
+                Log::shareContext([
+                    'memory_peak_usage' => memory_get_peak_usage(),
+                    'memory_usage' => memory_get_usage(),
+                ]);
+            },
+            CommandFinished::class => static function (CommandFinished $event): void {
+                Log::shareContext([
+                    'memory_peak_usage' => memory_get_peak_usage(),
+                    'memory_usage' => memory_get_usage(),
                 ]);
             },
         ];
