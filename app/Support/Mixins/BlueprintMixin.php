746042b3d71f28cbe53c0e82392e31ec7d8886a2
guanguans <ityaozm@gmail.com>
2024-11-20 10:04:51 +0800

    feat(BlueprintMixin): Add money column definition method
    - Introduce a new method 'money' in BlueprintMixin for defining decimal columns.
    - The method allows customization of total, places, unsigned attribute, and default value.
    - This enhances database schema flexibility for financial data.

--- app/Support/Mixins/BlueprintMixin.php
+++ app/Support/Mixins/BlueprintMixin.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 
 namespace App\Support\Mixins;
 
+use Illuminate\Database\Schema\ColumnDefinition;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Fluent;
 
@@ -21,6 +22,32 @@ use Illuminate\Support\Fluent;
 class BlueprintMixin
 {
     /**
+     * @see https://xammar-aldwayma.me/blog/avoid-duplication-in-laravel-migrations-with-custom-blueprint-macros
+     */
+    public function money(): \Closure
+    {
+        return function (
+            string $column,
+            int $total = 16,
+            int $places = 2,
+            bool $unsigned = false,
+            $default = null
+        ): ColumnDefinition {
+            $columnDefinition = $this->decimal($column, $total, $places);
+
+            if ($unsigned) {
+                $columnDefinition->unsigned();
+            }
+
+            if ($default !== null) {
+                $columnDefinition->default($default);
+            }
+
+            return $columnDefinition;
+        };
+    }
+
+    /**
      * Add a comment to the table.
      *
      * ```php
@@ -29,7 +56,7 @@ class BlueprintMixin
      * });
      * ```
      */
-    public function comment(): callable
+    public function comment(): \Closure
     {
         /*
          * @param  string  $comment
@@ -38,7 +65,7 @@ class BlueprintMixin
         return fn ($comment) => $this->addCommand('tableComment', ['comment' => $comment]);
     }
 
-    public function hasIndex(): callable
+    public function hasIndex(): \Closure
     {
         return function (string $index): bool {
             $schemaManager = Schema::getConnection()->getDoctrineSchemaManager();
@@ -47,7 +74,7 @@ class BlueprintMixin
         };
     }
 
-    public function dropIndexIfExists(): callable
+    public function dropIndexIfExists(): \Closure
     {
         return function (string $index): Fluent {
             /** @var \Illuminate\Database\Schema\Blueprint $this */
