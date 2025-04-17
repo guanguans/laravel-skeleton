c2c473c42baad33f3e60c8fe143646a1f18dc789
guanguans <ityaozm@gmail.com>
2025-04-17 16:37:55 +0800

    refactor(app-support): remove unused middleware and simplify mixin
    - Removed `TencentAdsRateLimiterMiddleware` as it was no longer in use.
    - Simplified `CommandMixin` by removing the `task` method and its associated logic.
    - Updated `.ai-commit.json` to streamline the `generators` configuration, removing redundant details.
    - Adjusted `phpstan.neon` configuration to reflect file removals and to update error handling rules.

--- app/Support/Mixins/CommandMixin.php
+++ app/Support/Mixins/CommandMixin.php
@@ -33,69 +33,6 @@ use Symfony\Component\Process\Process;
 #[Mixin(Command::class)]
 class CommandMixin
 {
-    /**
-     * ```php
-     * $this->task('Successful task.', function (){
-     *     return true;
-     * });.
-     *
-     * $this->task('Failed task.', function (){
-     *     return false;
-     * });
-     *
-     * // Specify a 3rd parameter for a custom loading message
-     * // Default is `loading...`
-     * $this->task('Long task.', function (){
-     *     sleep(3);
-     *
-     *     return true;
-     * }, 'sleeping...');
-     * ```
-     */
-    public function task(): callable
-    {
-        /*
-         * Performs the given task, outputs and
-         * returns the result.
-         *
-         * @param  string $title
-         * @param  callable|null $task
-         *
-         * @return bool With the result of the task.
-         */
-        return function (string $title, $task = null, $loadingText = 'loading...'): bool {
-            $this->output->write("$title: <comment>{$loadingText}</comment>");
-
-            if (null === $task) {
-                $result = true;
-            } else {
-                try {
-                    $result = false !== $task();
-                } catch (\Throwable) {
-                    $result = false;
-                }
-            }
-
-            if ($this->output->isDecorated()) { // Determines if we can use escape sequences
-                // Move the cursor to the beginning of the line
-                $this->output->write("\x0D");
-
-                // Erase the line
-                $this->output->write("\x1B[2K");
-            } else {
-                $this->output->writeln(''); // Make sure we first close the previous line
-            }
-
-            $this->output->writeln(
-                "$title: ".($result ? '<info>✔</info>' : '<error>failed</error>')
-            );
-
-            throw_if(isset($taskException), $taskException);
-
-            return $result;
-        };
-    }
-
     public function consoleLogger(): \Closure
     {
         return fn (
