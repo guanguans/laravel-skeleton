6a3c6bd3078e32ba321e9b2c1682664bc4e9707a
guanguans <ityaozm@gmail.com>
2025-04-24 16:26:38 +0800

    fix(app-service-provider): refine class filtering logic
    - Simplify the `classes` method logic by introducing a callback for filtering.
    - Replace hardcoded filtering with an inline callback for better readability.
    - Adjust conditions to filter classes more explicitly and refine indexation.
    - Comment out unused legacy logic for managing facade references.
    - Replace `classes` method for mixins discovery with the `Discover::in` approach.
    - Ensure compatibility and reduce unnecessary computations in the `tap` method.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,8 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Traits\Conditionable;
 use Illuminate\Support\Traits\EnumeratesValues;
 use Illuminate\Support\Traits\Macroable;
-use Laragear\Discover\Facades\Discover;
+use Laragear\Discover\Facades\Discover as LaragearDiscover;
+use Spatie\StructureDiscoverer\Discover as SpatieDiscover;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -55,38 +56,37 @@ class AppServiceProvider extends ServiceProvider
         //     ->classes()
         //     ->dd();
 
-        classes()
-            ->filter(function (string $class) {
-                return str($class)->is([
-                    Model::class,
-                    'Illuminate\\*',
-                ]) && !str($class)->is([
-                    Str::class,
-                    Arr::class,
-                    Carbon::class,
-                    Number::class,
-                    Enumerable::class,
-                    EnumeratesValues::class,
-                ]);
-            })
-            ->map(static function (string $class) {
-                if (is_subclass_of($class, Facade::class)) {
-                    $prefix = ' * @see \\';
-
-                    $seeClass = str((new \ReflectionClass($class))->getDocComment())
-                        ->explode(\PHP_EOL)
-                        ->filter(fn (string $line) => str($line)->startsWith($prefix))
-                        ->pipe(fn (Collection $collection): string => Str::remove($prefix, $collection->firstOrFail()));
-
-                    return [
-                        new \ReflectionClass($class),
-                        new \ReflectionClass($seeClass),
-                    ];
-                }
-
-                return new \ReflectionClass($class);
-            })
-            ->flatten()
+        classes(function (string $file, string $class) {
+            return str($class)->is([
+                Model::class,
+                'Illuminate\\Support\\*',
+            ]) && !str($class)->is([
+                Str::class,
+                Arr::class,
+                Carbon::class,
+                Number::class,
+                Enumerable::class,
+                EnumeratesValues::class,
+            ]);
+        })
+            // ->map(static function (string $class) {
+            //     if (is_subclass_of($class, Facade::class)) {
+            //         $prefix = ' * @see \\';
+            //
+            //         $seeClass = str((new \ReflectionClass($class))->getDocComment())
+            //             ->explode(\PHP_EOL)
+            //             ->filter(fn (string $line) => str($line)->startsWith($prefix))
+            //             ->pipe(fn (Collection $collection): string => Str::remove($prefix, $collection->firstOrFail()));
+            //
+            //         return [
+            //             new \ReflectionClass($class),
+            //             new \ReflectionClass($seeClass),
+            //         ];
+            //     }
+            //
+            //     return new \ReflectionClass($class);
+            // })
+            // ->flatten()
             ->mapWithKeys(fn (\ReflectionClass $reflectionClass) => [
                 $reflectionClass->getName() => collect($reflectionClass->getMethods(\ReflectionMethod::IS_STATIC))
                     ->filter(
@@ -112,12 +112,20 @@ class AppServiceProvider extends ServiceProvider
             ])
             ->filter()
             // ->dd()
-            ->tap();
+            ->tap(function (): void {});
     }
 
     private function registerMixins(): void
     {
-        classes(static fn (string $file, string $class): bool => str($class)->is('App\\Support\\Mixins\\*'))
+        // classes(static fn (string $file, string $class): bool => str($class)->is('App\\Support\\Mixins\\*'))
+        // LaragearDiscover::in('Support/Mixins')
+        collect(
+            SpatieDiscover::in(__DIR__.'/../Support/Mixins/')
+                // ->parallel()
+                // ->useReflection(__DIR__.'/../Support/Mixins/', 'App\\Support\\Mixins')
+                ->classes()
+                ->get()
+        )->mapWithKeys(static fn (string $class): array => [$class => new \ReflectionClass($class)])
             // ->keys()
             // ->dd()
             ->each(static function (\ReflectionClass $mixinReflectionClass, string $mixinClass): void {
@@ -133,7 +141,7 @@ class AppServiceProvider extends ServiceProvider
 
     private function registerProviders(): void
     {
-        Discover::in('Providers')
+        LaragearDiscover::in('Providers')
             ->instancesOf(ServiceProvider::class)
             ->classes()
             ->keys()
