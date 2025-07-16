e948b762164914bf83f79ef5f71f6b1702085302
guanguans <ityaozm@gmail.com>
2025-07-16 12:13:22 +0800

    feat(php-cs-fixer): add ShfmtFixer for shell script formatting
    - Introduce ShfmtFixer class extending AbstractToolFixer
    - Configure default program to run 'dotenv-linter fix'
    - Support shell script file extensions: sh, zsh, bash, fish
    - Enhance codebase by adding automated shell script formatting support

---
+++ app/Support/PhpCsFixer/Fixer/BladeFormatterFixer.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2021-2025 guanguans<ityaozm@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/guanguans/laravel-skeleton
+ */
+
+namespace App\Support\PhpCsFixer\Fixer;
+
+/**
+ * @see https://github.com/shufo/blade-formatter
+ */
+final class BladeFormatterFixer extends AbstractToolFixer
+{
+    #[\Override]
+    protected function defaultProgram(): array
+    {
+        return ['dotenv-linter', 'fix'];
+    }
+
+    #[\Override]
+    protected function supportsExtensions(): array
+    {
+        return ['blade.php'];
+    }
+}
